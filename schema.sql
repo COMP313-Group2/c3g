597f37555a49d63f3ba418b1f899b55a643a2af8
Hassan Shabbir <hassan149367@gmail.com>
2021-09-27 23:03:53 +0000

    init c3g

---
+++ schema.sql
@@ -0,0 +1,13 @@
+-- SQLite3 db
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS quiz;
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  email TEXT UNIQUE NOT NULL, -- email can also be used to identify a user
+  password TEXT NOT NULL
+);
+
+-- Initialize data in db with example data
+INSERT INTO user VALUES (0, "hassan", "hassan149367@gmail.com", "password");
