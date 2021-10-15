e4a10e4908b9a616a7dbf47ffc7dec09953b49c3
Hassan Shabbir <hassan149367@gmail.com>
2021-10-15 18:29:00 -0400

    added db and starting flask info

--- app.py
+++ app.py
@@ -1,10 +1,68 @@
-from flask import Flask, render_template
+# Flask web framework
+from flask import Flask, render_template, g
+# Database
+import sqlite3
+# from app import create_app
 
 app = Flask(__name__)
 
+
+################################################################################
+# Database Configuration and Helpers
+
+# SQLite database storage file
+DATABASE = 'database.db'
+
+# Connect to SQLite and return dicts from db
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+
+    def make_dicts(cursor, row):
+        return dict((cursor.description[idx][0], value)
+                    for idx, value in enumerate(row))
+
+    db.row_factory = make_dicts
+    return db
+
+
+# Query helper function
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+# Db connection close helper function
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+# Db schema and data initializer
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+init_db()
+
+
+
+################################################################################
+# Routes
+
 @app.route("/")
 def home():
-    return render_template('index.html')
+    users = query_db('SELECT * FROM user')
+    return render_template('index.html', users=users)
 
 @app.route("/space_pong")
 def space_pong():
@@ -21,6 +79,24 @@ def excaliburs_quest():
 @app.route("/potato_tomato")
 def potato_tomato():
     return render_template('potato_tomato/index.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ##################################################
 # WebGL test
